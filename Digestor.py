264afe57b5916b6becb794f36127085b227d58e9
Agustin <agustin.gambirassi@hasar.com>
2020-07-30 13:37:56 -0300

    log hora local

--- Digestor.py
+++ Digestor.py
@@ -22,7 +22,8 @@ class Digestor():
 		# El cursor va a tener todas las reglas que matcheen con el id del dispositivo	
 		cursor = self.mongo.find(json.loads(idDisp))
 		self.curTime = datetime.datetime.today()
-		logging.debug('[+] Hora del servidor ' + self.curTime)	
+		logging.debug('[+] Hora del servidor ')	
+		logging.debug(self.curTime)
 		for regla in cursor:
 			print(regla)
 			id2 = regla['consequences'][0]['id2']
